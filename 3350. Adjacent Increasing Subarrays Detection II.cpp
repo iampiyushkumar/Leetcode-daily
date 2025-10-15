37bc5da49515281a04cf3cbaf876bb713d76a7ec
Piyush kumar <147995982+iampiyushkumar@users.noreply.github.com>
2025-10-15 11:15:19 +0530

    Create 3350. Adjacent Increasing Subarrays Detection II.cpp

---
+++ 3350. Adjacent Increasing Subarrays Detection II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        int curr=1;
+        int prev=0;
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]<nums[i]) curr++;
+            else{
+                   prev=curr;
+                   curr=1;
+            }
+            ans=max(ans,max(curr/2,min(curr,prev)));
+        }
+        return ans;
+    }
+};
