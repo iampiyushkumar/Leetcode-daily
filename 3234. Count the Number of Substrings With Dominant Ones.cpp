5af4823a122cb43b8363be81e1514d077f97161c
Piyush kumar <147995982+iampiyushkumar@users.noreply.github.com>
2025-11-15 23:39:58 +0530

    Create 3234. Count the Number of Substrings With Dominant Ones.cpp

---
+++ 3234. Count the Number of Substrings With Dominant Ones.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // number of ones>=square of no of zeroes
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> left(n + 1, 0);
+        left[0] = -1;
+        left[1] = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (s[i - 1] == '0') {
+                left[i + 1] = i;
+            } else {
+                left[i + 1] = left[i];
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            int zeroes = 0;
+            if (s[i - 1] == '0') {
+                zeroes++;
+            }
+
+            int j = i;
+
+            while (zeroes * zeroes <= n && j > 0) {
+                int ones = i - left[j] - zeroes;
+
+                if (zeroes * zeroes <= ones) {
+                    ans += min(j - left[j], ones - zeroes * zeroes + 1);
+                }
+                j = left[j];
+                zeroes++;
+            }
+        }
+
+        return ans;
+    }
+};
