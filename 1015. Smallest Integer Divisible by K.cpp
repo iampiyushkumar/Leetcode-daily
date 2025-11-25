cd238b8484d855cd50be6d6b2c8c59d6b0ee36b3
Piyush kumar <147995982+iampiyushkumar@users.noreply.github.com>
2025-11-25 11:59:50 +0530

    Implement solution for smallest repunit divisible by K

---
+++ 1015. Smallest Integer Divisible by K.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+      int remainder=0;
+      for(int len=1;len<=k;len++){
+          remainder=(remainder*10+1)%k;
+          if(remainder==0) return len;
+      }
+      return -1;
+    }
+};
